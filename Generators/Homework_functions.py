19935b2b8e94937ac81fbafcde26db1a0577e812
Andrii Kovalenko <andrey.kovalenko@comodo.com>
2018-08-19 19:32:01 +0300

    add dictAdd function

--- Generators/Homework_functions.py
+++ Generators/Homework_functions.py
@@ -25,14 +25,27 @@ def adder_name(**x):
     return summ
 
 def copyDict(dict):
-    if len(dict) == 0: return "Fuck"
+    # if len(dict) == 0: return "Fuck"
     temp = list(dict.keys())
     summ ={}
     for i in temp:
          summ[i]=dict[i]
+    print(type(summ))
     return summ
 # print (adder(1,2))
-print (adder_name(bad='10', ugly='2', good='5', gg='6'))
-new=copyDict({'bad':'10', 'ugly':'2', 'good':'5', 'gg':'6'})
-new['ugly']=11111
-print(new)
+def addDict(dict1, dict2):
+    summ_dict=copyDict(dict1)
+    temp = list(dict2.keys())
+    for i in temp:
+         summ_dict[i]=dict2[i]
+    return summ_dict
+a={'a':1}
+b={'b':2, 'a':3}
+print(addDict(b, a))
+
+
+# print (addDict({'a':1}, {'b':2}))
+# print (adder_name(bad='10', ugly='2', good='5', gg='6'))
+# new=copyDict({'bad':'10', 'ugly':'2', 'good':'5', 'gg':'6'})
+# new['ugly']=11111
+# print(new)
