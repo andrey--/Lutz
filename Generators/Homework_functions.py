b21a975c6f442234c3fe995d6e97b8138e722b9f
Andrii Kovalenko <andrey.kovalenko@comodo.com>
2018-08-26 17:29:06 +0300

    add isprime function

--- Generators/Homework_functions.py
+++ Generators/Homework_functions.py
@@ -1,3 +1,6 @@
+from math import sqrt
+
+
 def adder(*x):
     if len(x)==0 : return
     summ=x[0]
@@ -34,14 +37,44 @@ def copyDict(dict):
     return summ
 # print (adder(1,2))
 def addDict(dict1, dict2):
+    if (type(dict1)==list) and (type(dict2)==list):
+        return dict1+dict2
+    if (type(dict1)!=list) or (type(dict2)!=list):
+        return
     summ_dict=copyDict(dict1)
     temp = list(dict2.keys())
     for i in temp:
          summ_dict[i]=dict2[i]
     return summ_dict
+def sqrtgener(input):
+    output=list(map(sqrt,input))
+    output=[sqrt(x) for x in input]
+    return output
+def isprime(input):
+    for x in range(2, int(sqrt(input))+1):
+        if input%x==0:
+            print(input, 'has factor', x)
+            break
+    else:
+        print(input, 'is prime')
+
+isprime(1543235325332332)
+
+# print(int(sqrt(15)))
+# isprime(-13)
+# isprime(15003453453453453535)
+# isprime(15.0)
+
 a={'a':1}
-b={'b':2, 'a':3}
-print(addDict(b, a))
+c=[1,2]
+d=[1]
+e=[2,4,9,16,25]
+# print(sqrtgener(e))
+#
+# if type(d)==list:
+#     print ("true")
+# b={'b':2, 'a':3}
+# print(addDict(a, c))
 
 
 # print (addDict({'a':1}, {'b':2}))
