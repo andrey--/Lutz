46595924523f45f9f415d94020ed792b0942056d
Andrey Kovalenko <a.y.kovalenko@lohika.com>
2018-12-09 20:28:54 +0200

    Classes

--- Generators/importer.py
+++ Generators/importer.py
@@ -1,11 +1,16 @@
+import person
+
+
+
+
 # import Mymap
-import Homework_functions
-import Mymap
+# import Homework_functions
+# import Mymap
 # from Homework_functions import xxx, yyy
 
 
-print(Homework_functions.xxx)
-print(Homework_functions.yyy)
+# print(Homework_functions.xxx)
+# print(Homework_functions.yyy)
 # Homework_functions.printer()
 # Homework_functions.printeryyy()
 # print(xxx)
--- Generators/person.py
+++ Generators/person.py
@@ -3,3 +3,34 @@ class Person:
         self.name=name
         self.job=job
         self.pay=pay
+    def __str__(self):
+        return '[Person: %s, %s]'%(self.name, self.pay)
+    def lastName(self):
+        return self.name.split()[-1]
+    def giveRaise(self, percent):
+        self.pay=int(self.pay*(1+percent))
+
+
+class Manager(Person):
+    def giveRaise(self, percent, bonus=.10):
+        Person.giveRaise(self, percent+bonus)
+    # def anotherOne(self):
+    #     print("PPPP")
+    def __init__(self, name, pay):
+        Person.__init__(self, name, "mgr", pay)
+class ttt:
+    def giveRaise(self, a):
+        self.pay=10
+    def __str__(self):
+        return '[PAY: %s]'%(self.pay)
+
+if __name__=='__main__':
+    bob=Person("Bob", pay=1000)
+    sue=Person("Sue", job="Engineer")
+    tom=Manager("Ton jr", 50000)
+andrey=ttt()
+def test(*employees):
+    for employee in employees:
+        employee.giveRaise(.10)
+        print(employee)
+test(tom, bob, sue, andrey)
