9fad2a83f4046f777262f3f6f3d84389b1fd210e
Andrey Kovalenko <a.y.kovalenko@lohika.com>
2018-12-16 18:35:38 +0200

    ClassTools - gettig class tree

--- Generators/classtools.py
+++ Generators/classtools.py
@@ -5,6 +5,14 @@ class AttrDisplay:
         for key in sorted(self.__dict__):
             attrs.append('%s=%s' % (key, getattr(self, key)))
         return ', '.join(attrs)
+    def gatherClassTree(self):
+        classChain=''
+        if len(self.__class__.__bases__)==1 and self.__class__.__bases__[0]=="<class 'object'>":
+            return classChain + self.__class__.__bases__[0]
+        else:
+            for i in 
+
+
     def __str__(self):
         return '[%s: %s]' % (self.__class__.__name__, self.gatherAttrs())
 
@@ -17,7 +25,18 @@ if __name__=='__main__':
             TopTest.count+=2
     class SubTest(TopTest):
         pass
+    class Third(SubTest):
+        pass
 
     X, Y = TopTest(), SubTest()
-    print (X)
-    print (Y)
+    # print (X)
+    # print (Y)
+    z=Third()
+    # print(Y.__class__.__bases__)
+    f=AttrDisplay()
+    # print(z.__class__.__bases__)
+    print(f.__class__.__bases__[0])
+    # z=X.__class__.__bases__[0]
+    # print(z.__class__.__name__)
+    # for i in Y.__class__.__bases__:
+    #     print(i.__name__)
