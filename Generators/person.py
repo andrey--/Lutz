cb18471eceadd2964a52b39dafba103d8accd296
Andrey Kovalenko <a.y.kovalenko@lohika.com>
2018-12-16 16:48:30 +0200

    ClassTools

--- Generators/person.py
+++ Generators/person.py
@@ -1,10 +1,12 @@
-class Person:
+import classtools
+class Person(classtools.AttrDisplay):
     def __init__(self, name, job=None, pay=0):
         self.name=name
         self.job=job
         self.pay=pay
-    def __str__(self):
-        return '[Person: %s, %s]'%(self.name, self.pay)
+    # def __str__(self):
+    #     return '[%s: %s, %s]'%(self.__class__.__name__, self.name, self.pay)
+
     def lastName(self):
         return self.name.split()[-1]
     def giveRaise(self, percent):
@@ -18,17 +20,17 @@ class Manager(Person):
     #     print("PPPP")
     def __init__(self, name, pay):
         Person.__init__(self, name, "mgr", pay)
-class ttt:
+class ttt(Person):
     def giveRaise(self, a):
         self.pay=10
-    def __str__(self):
-        return '[PAY: %s]'%(self.pay)
+    # def __str__(self):
+    #     return '[PAY: %s]'%(self.pay)
 
 if __name__=='__main__':
     bob=Person("Bob", pay=1000)
     sue=Person("Sue", job="Engineer")
-    tom=Manager("Ton jr", 50000)
-andrey=ttt()
+    tom=Manager("Tom jr", 50000)
+andrey=ttt('andrey')
 def test(*employees):
     for employee in employees:
         employee.giveRaise(.10)
