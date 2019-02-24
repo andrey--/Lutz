e7da261baef0de4ab93aca1d5adb4ea980d5284f
akovalenko-lohika-tix <akovalenko@lohika.com>
2019-02-24 21:43:48 +0200

    Playing with __str__

--- Generators/importer.py
+++ Generators/importer.py
@@ -1,17 +1,29 @@
-import person
+def reclist(instance):
+    valueToReturn = instance.__class__.__qualname__ + "\n"
+    for elem in instance.__dict__:
+        valueToReturn += "{0}={1}\n".format(elem, getattr(instance, elem))
+    return valueToReturn
 
 
+class clas1:
+    rrrrr = 1
+
+    def __str__(self):
+        return reclist(self)
+
+    def aaa(self):
+        pass
+
+    def __init__(self):
+        self.fff = 5
+        self.new = 123
 
 
-# import Mymap
-# import Homework_functions
-# import Mymap
-# from Homework_functions import xxx, yyy
-
-
-# print(Homework_functions.xxx)
-# print(Homework_functions.yyy)
-# Homework_functions.printer()
-# Homework_functions.printeryyy()
-# print(xxx)
-# print(yyy)
+asd = clas1()
+asd.ppp = 56
+asd.iii = lambda r: r + 10
+print(asd)
+print(clas1)
+# asd.ppp=56
+# def asdsada():
+#     pass
